b300a1477aaab2da9fbedfb650e5a3681cc5fe0e
Dimitar <wopz1337@gmail.com>
2022-12-09 13:52:39 +0200

    Import & Render Conversation Component

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,10 +1,25 @@
+import { Box, Stack, useTheme } from "@mui/material";
 import Chats from "./Chats";
-
+import Conversation from "../../components/Conversation";
 const GeneralApp = () => {
+  const theme = useTheme();
   return (
-    <>
+    <Stack direction="row" sx={{ width: "100%" }}>
       <Chats />
-    </>
+
+      <Box
+        sx={{
+          height: "100%",
+          width: "calc(100vw - 420px )",
+          backgroundColor:
+            theme.palette.mode === "light"
+              ? "#FFF"
+              : theme.palette.background.default,
+        }}
+      >
+        <Conversation />
+      </Box>
+    </Stack>
   );
 };
 
