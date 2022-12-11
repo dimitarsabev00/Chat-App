39494f6260d6e48316cb788eef06385337ce08c7
Dimitar <wopz1337@gmail.com>
2022-12-11 15:33:02 +0200

    Import & Render Contact Component With Conditional Rendering

--- src/pages/dashboard/GeneralApp.js
+++ src/pages/dashboard/GeneralApp.js
@@ -1,8 +1,13 @@
 import { Box, Stack, useTheme } from "@mui/material";
 import Chats from "./Chats";
 import Conversation from "../../components/Conversation";
+import Contact from "../../components/Contact";
+import { useSelector } from "react-redux";
 const GeneralApp = () => {
   const theme = useTheme();
+
+  const { sideBar } = useSelector((state) => state.app);
+
   return (
     <Stack direction="row" sx={{ width: "100%" }}>
       <Chats />
@@ -10,7 +15,7 @@ const GeneralApp = () => {
       <Box
         sx={{
           height: "100%",
-          width: "calc(100vw - 420px )",
+          width: sideBar.open ? `calc(100vw - 740px )` : "calc(100vw - 420px )",
           backgroundColor:
             theme.palette.mode === "light"
               ? "#f0f4fa"
@@ -19,6 +24,7 @@ const GeneralApp = () => {
       >
         <Conversation />
       </Box>
+      {sideBar.open && <Contact />}
     </Stack>
   );
 };
