3a588c5d0f77d93e37ff8095d289a12d517913f7
Dimitar <wopz1337@gmail.com>
2022-12-13 21:24:43 +0200

    Added Login Component

---
+++ src/pages/Login.js
@@ -0,0 +1,38 @@
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+const Login = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        margin: "0 auto",
+        width: "80%",
+        minHeight: "1000px",
+      }}
+    >
+      <Typography>Login</Typography>
+      <form
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "15px",
+          margin: "1rem",
+        }}
+      >
+        <TextField type="email" placeholder="email" />
+        <TextField type="password" placeholder="password" />
+
+        <Button variant="contained">Sign In</Button>
+      </form>
+      <Typography>
+        You do have an account?
+        <Link to="/register">Register</Link>
+      </Typography>
+    </Box>
+  );
+};
+
+export default Login;
