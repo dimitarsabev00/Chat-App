856240724357e8233290e38238a0c584637f3755
Dimitar <wopz1337@gmail.com>
2022-12-14 14:16:20 +0200

    Added Firebase Storage In Firebase Config File

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyADUQbgsjvf7A2puRmh2YYJJnOpHLr-Pvs",
   authDomain: "chat-app-99af3.firebaseapp.com",
@@ -16,3 +17,4 @@ const firebaseConfig = {
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
