b0cd19b907d10425321ef661776feae3ef82f331
Dimitar <wopz1337@gmail.com>
2022-12-14 14:19:34 +0200

    Added Register Functionality

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,7 +1,49 @@
 import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { Image } from "phosphor-react";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth, db, storage } from "../firebaseConfig";
 const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [file, setFile] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+  const handleRegisterUserSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const imageRef = ref(storage, `avatars/${displayName}`);
+      uploadBytes(imageRef, file).then(() => {
+        getDownloadURL(imageRef).then(async (downloadURL) => {
+          await updateProfile(response.user, {
+            displayName,
+            photoURL: downloadURL,
+          });
+          await setDoc(doc(db, "users", response.user.uid), {
+            userID: response.user.uid,
+            displayName,
+            email,
+            password,
+            photoURL: downloadURL,
+          });
+          await setDoc(doc(db, "userChats", response.user.uid), {});
+          navigate("/app");
+        });
+      });
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
     <Box
       sx={{
@@ -22,11 +64,40 @@ const Register = () => {
           gap: "15px",
           margin: "1rem",
         }}
+        onSubmit={handleRegisterUserSubmit}
       >
-        <TextField type="text" placeholder="display name" />
-        <TextField type="email" placeholder="email" />
-        <TextField type="password" placeholder="password" />
-        <TextField sx={{ display: "none" }} type="file" id="file" />
+        <TextField
+          type="text"
+          placeholder="display name"
+          value={displayName}
+          onChange={(e) => {
+            setDisplayName(e.target.value);
+          }}
+        />
+        <TextField
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <TextField
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
+        <TextField
+          sx={{ display: "none" }}
+          type="file"
+          id="file"
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+        />
         <InputLabel
           htmlFor="file"
           style={{ display: "flex", alignItems: "center", gap: "5px" }}
@@ -34,7 +105,10 @@ const Register = () => {
           <Image size={40} />
           <Box>Add an avatar</Box>
         </InputLabel>
-        <Button variant="contained">Sign Up</Button>
+        <Button variant="contained" type="submit">
+          Sign Up
+        </Button>
+        {error && <span>Somethink went wrong</span>}
       </form>
       <Typography>
         You do have an account?{" "}
