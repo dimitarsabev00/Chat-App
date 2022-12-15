a74a5f0839f2a317397286b391ef58ab8b8ca539
Dimitar <wopz1337@gmail.com>
2022-12-15 12:27:02 +0200

    Added Conditional Rendering For SideBar Component

--- src/components/layouts/index.js
+++ src/components/layouts/index.js
@@ -1,16 +1,18 @@
 import { Stack } from "@mui/material";
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { UserAuth } from "../../contexts/AuthContext";
 import useResponsive from "../../hooks/useResponsive";
 import SideBar from "./SideBar";
 
 const Layout = () => {
   const isDesktop = useResponsive("up", "md");
-
+  const { user } = UserAuth();
+  console.log(user);
   return (
     <>
       <Stack direction="row">
-        {isDesktop && <SideBar />}
+        {isDesktop && user && <SideBar />}
 
         <Outlet />
       </Stack>
