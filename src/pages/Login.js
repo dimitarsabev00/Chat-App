1f2f95524a51fdf79ca8d568238d19617cef4167
Dimitar <wopz1337@gmail.com>
2022-12-18 22:57:48 +0200

    Added Login Functionality

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,6 +1,24 @@
 import { Box, Button, TextField, Typography } from "@mui/material";
-import { Link } from "react-router-dom";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { auth } from "../firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
+  const handleLoginUser = async (e) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/");
+    } catch (error) {
+      setError(true);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -21,11 +39,29 @@ const Login = () => {
           gap: "15px",
           margin: "1rem",
         }}
+        onSubmit={handleLoginUser}
       >
-        <TextField type="email" placeholder="email" />
-        <TextField type="password" placeholder="password" />
+        <TextField
+          type="email"
+          placeholder="email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <TextField
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        />
 
-        <Button variant="contained">Sign In</Button>
+        <Button variant="contained" type="submit">
+          Sign In
+        </Button>
+        {error && <span>Somethink went wrong</span>}
       </form>
       <Typography>
         You do have an account?{" "}
