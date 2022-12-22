61db0d9f69a3b6dd3d3af7f9ecb46e710cf01945
Dimitar <wopz1337@gmail.com>
2022-12-22 19:14:19 +0200

    Optimize Imports In AuthContext

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,6 +1,5 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useContext } from "react";
 
-import { useContext } from "react";
 import { auth } from "../firebaseConfig";
 import { onAuthStateChanged } from "firebase/auth";
 
