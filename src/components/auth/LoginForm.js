62946a1de0be02e4f3cf360b4b857237a0bb6c12
Dimitar <wopz1337@gmail.com>
2022-12-28 19:08:59 +0200

    Added LoginForm Component

---
+++ src/components/auth/LoginForm.js
@@ -0,0 +1,121 @@
+import React from "react";
+import { useState } from "react";
+import FormProvider from "../hook-form/FormProvider";
+import * as Yup from "yup";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import {
+  Alert,
+  Button,
+  IconButton,
+  InputAdornment,
+  Link,
+  Stack,
+} from "@mui/material";
+import { RHFTextField } from "../hook-form";
+import { Eye, EyeSlash } from "phosphor-react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../../firebaseConfig";
+import { doc, updateDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+const LoginForm = () => {
+  const [showPasssword, setShowPassword] = useState(false);
+
+  const LoginSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("Email is required")
+      .email("Email must be a valid email adress"),
+    password: Yup.string().required("Password is required"),
+  });
+
+  const defaultValues = {
+    email: "",
+    password: "",
+  };
+  const methods = useForm({
+    resolver: yupResolver(LoginSchema),
+    defaultValues,
+  });
+  const {
+    reset,
+    setError,
+    handleSubmit,
+    formState: { errors, isSubmitting, isSubmitSuccessful },
+  } = methods;
+  const navigate = useNavigate();
+  const onSubmit = async (data) => {
+    try {
+      //submit data back-end
+      const result = await signInWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      await updateDoc(doc(db, "users", result.user.uid), { isOnline: true });
+
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      reset();
+      setError("afterSubmit", {
+        ...error,
+        message: error.message,
+      });
+    }
+  };
+  return (
+    <FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
+      <Stack spacing={3}>
+        {!!errors.afterSubmit && (
+          <Alert severity="error">{errors.afterSubmit.message}</Alert>
+        )}
+
+        <RHFTextField name="email" label="Email address" />
+        <RHFTextField
+          name="password"
+          label="Password"
+          type={showPasssword ? "text" : "password"}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment>
+                <IconButton
+                  onClick={() => {
+                    setShowPassword(!showPasssword);
+                  }}
+                >
+                  {showPasssword ? <Eye /> : <EyeSlash />}
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
+        />
+      </Stack>
+      <Stack alignItems={"flex-end"} sx={{ my: 2 }}>
+        <Link variant="body2" color="inherit" underline="always">
+          Forgot Password?
+        </Link>
+      </Stack>
+      <Button
+        fullWidth
+        color="inherit"
+        size="large"
+        type="submit"
+        variant="contained"
+        sx={{
+          bgcolor: "text.primary",
+          color: (theme) =>
+            theme.palette.mode === "light" ? "common.white" : "grey.800",
+          "&:hover": {
+            bgcolor: "text.primary",
+            color: (theme) =>
+              theme.palette.mode === "light" ? "common.white" : "grey.800",
+          },
+        }}
+      >
+        {isSubmitting ? "Loading.." : "Sign In"}
+      </Button>
+    </FormProvider>
+  );
+};
+
+export default LoginForm;
