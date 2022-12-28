3753b6060e34ab5043a323bad521a6a5535f8889
Dimitar <wopz1337@gmail.com>
2022-12-28 19:07:52 +0200

    Added RHFTextField

---
+++ src/components/hook-form/RHFTextField.js
@@ -0,0 +1,23 @@
+//form
+import { useFormContext, Controller } from "react-hook-form";
+//mui
+import { TextField } from "@mui/material";
+
+export default function RHFTextField({ name, helperText, ...other }) {
+  const { control } = useFormContext();
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({ field, fieldState: { error } }) => (
+        <TextField
+          {...field}
+          fullWidth
+          error={!!error}
+          helperText={error ? error.message : helperText}
+          {...other}
+        />
+      )}
+    />
+  );
+}
